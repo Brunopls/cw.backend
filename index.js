d7f9eeb5223c9c501adfa8d7f98824c37ae4ab77
Bruno Correia <oliveirb@coventry.ac.uk>
2020-11-26 16:47:33 +0000

    Changed JSDocs format.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = new Koa();
 const mongoose = require("mongoose");
 const info = require("./config");
 
+
 mongoose.Promise = global.Promise;
 mongoose.connect(info.config.database, info.config.mongoOptions, (err) => {
   if (err) throw err;
@@ -13,4 +14,6 @@ mongoose.connect(info.config.database, info.config.mongoOptions, (err) => {
 mongoose.set("useCreateIndex", true);
 mongoose.set("useFindAndModify", false);
 
-app.listen(info.config.port, () => console.log(`Listening on port ${info.config.port}.`));
+app.listen(info.config.port, () =>
+  console.log(`Listening on port ${info.config.port}.`)
+);
--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -2,6 +2,15 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
+/**
+ * @class Users
+ * @property {String} email unique email, used for logging in
+ * @property {String} password hashed password
+ * @property {String} passwordSalt password salt used for hashing
+ * @property {String} fullName full name
+ * @property {ObjectId} role role
+ * 
+ */
 const UsersSchema = new Schema({
   email: {
     type: String,
@@ -25,5 +34,45 @@ const UsersSchema = new Schema({
   },
 });
 
-const Users = mongoose.model("Users", UsersSchema);
-module.exports = Users;
+
+UsersSchema.statics = {
+
+  /**
+   * Get user by ID
+   * @memberof Users
+   * @async
+   * @param {ObjectId} id User ID
+   * @returns {Promise<User>} User object
+   */
+  async getByID(id) {
+    try {
+      return this.findById(id)
+        .exec()
+        .then((user) => {
+          return user;
+        });
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  },
+
+  /**
+   * Get all users
+   * @memberof Users
+   * @async
+   * @returns {Promise<User[]>} Array of User objects
+   */
+  async getAll() {
+    try {
+      return this.find({})
+        .exec()
+        .then((users) => {
+          return users;
+        });
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  },
+};
+
+module.exports = mongoose.model("Users", UsersSchema);
